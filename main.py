c619fc00d182e17d5ab3277ba51747a034f4e684
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-19 12:54:28 -0700

    Fixed a typing error.
    I replaced a . with a , in the helplist part.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ x = 1
 y = 2
 z = 3
 alphabet = ['a''b''c''d''e''f''g''h''i''j''k''l''m''n''o''p''q''r''s''t''u''v''w''x''y''z']
-helplist = ['Reboot your python, ''Install availible updates, ''Make sure your using Python 3.*.''And if none of that works then you may have to reinstall python.']
+helplist = ['Reboot your python, ''Install availible updates, ''Make sure your using Python 3.*,''And if none of that works then you may have to reinstall python.']
 # Outputting stuff
 print("Python3 Varible Test")
 print("This will print bolth List Variables and Normal Variables.")
