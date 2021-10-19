1f751af999f40c8d2ff10be5a6317569d069d431
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-19 13:17:37 -0700

    Added setup.py
    Added a setup file so i can publish the repo as a package on the Python Package Registry

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setup
+
+setup(name='PythonVariableTest',
+      version='1.0',
+      description='Test Variables in python 3.*!',
+      author='Nathaniel Carman',
+      author_email='478018@bsd48.org',
+      url='https://github.com/bupboi1337/python3-variable-test',
+      packages=['PythonVariableTest'],
+     )
