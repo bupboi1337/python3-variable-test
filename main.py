548b8b090ddd624d7901a1562a69bb792c522185
Nathaniel Carman <51461039+bupboi1337@users.noreply.github.com>
2021-10-19 12:49:11 -0700

    Made the test way better
    I added a extra part that will say a list containing the entire alphabet, Added Troubleshooting tips, Added some extra text, And added some notes in the code.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-print("Python3 Varible Test")
-
+# Making Variables
 x = 1
 y = 2
 z = 3
-
-print (x, y, z)
-print("If the output was 1 2 3 then the test worked.")
+alphabet = ['a''b''c''d''e''f''g''h''i''j''k''l''m''n''o''p''q''r''s''t''u''v''w''x''y''z']
+helplist = ['Reboot your python, ''Install availible updates, ''Make sure your using Python 3.*.''And if none of that works then you may have to reinstall python.']
+# Outputting stuff
+print("Python3 Varible Test")
+print("This will print bolth List Variables and Normal Variables.")
+print("Now starting test")
+print(x, y, z)
+print(alphabet)
+print("If the output was 1 2 3 and then it said the entire alphabet then the test worked.")
+# print troubleshooting list
+print("If it did not work try the following: ")
+print(*helplist, sep='\n')
